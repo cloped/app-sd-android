a8539b969d5e7685688a3f613c95bca58182efe2
Eduardo Lopes <eduardo.lopes@neemu.com>
2019-04-28 15:54:43 -0400

    json object

--- app/src/main/java/com/example/sistemas_distribuidos/MainActivity.java
+++ app/src/main/java/com/example/sistemas_distribuidos/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.sistemas_distribuidos;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -36,15 +40,31 @@ public class MainActivity extends AppCompatActivity {
             while((bytesRead = in.read(contents)) != -1) {
                 strFileContents += new String(contents, 0, bytesRead);
             }
-            mTextView.setText(strFileContents);
+
+
+            try {
+                JSONArray arr = new JSONArray(strFileContents);
+                mTextView.setText(arr.getJSONObject(2).getString("name"));
+                mTextView.setText(Double.toString(arr.getJSONObject(2).getDouble("area")));
+            } catch (Throwable t) {
+                mTextView.setText("Json error");
+            }
+
+
+            banco(arr);
         } catch (Exception e) {
             mTextView.setText("Error: " + e);
         } finally {
             urlConnection.disconnect();
         }
-
     }
 
+    public void eraseText(View view) {
+        final TextView mTextView = findViewById(R.id.api_response);
+        mTextView.setText("Empty");
+    }
 
+    public void banco(JSONArray arr) {
 
+    };
 }
